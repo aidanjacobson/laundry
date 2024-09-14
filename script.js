f08a10c294b8cb42d561497d100e29fc9f1ac939
aidanjacobson <survivalking4@gmail.com>
2024-09-13 19:50:36 -0700

    kdnbjklz

--- script.js
+++ script.js
@@ -63,6 +63,7 @@ function calculateFirstAndLastFinish() {
     console.log(firstFinish, lastFinish);
     config.nextFinish = firstFinish;
     config.lastFinish = lastFinish;
+    config.firstStart = firstStart;
     updateFinishBasedOnPrefs();
 }
 
